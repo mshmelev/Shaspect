04bdebb999745359f4922f2e16e2891a2db85a55
Max Shmelev <mshmelev@gmail.com>
2015-10-08 13:40:39 -0400

    Delete TODO list

--- ShaspectBuilder/ShaspectBuildTask.cs
+++ ShaspectBuilder/ShaspectBuildTask.cs
@@ -31,19 +31,6 @@ namespace Shaspect.Builder
                         AssemblyFile);
                 }
 
-                // TODO: specifying targets (properties, methods). Now done only for ctor. Don't forget about Exclude.
-                // TODO: specifying targets by name (Namespace1.Namespace2.Class.*). Don't forget about Exclude.
-                // TODO: Implement priorities (cut-through across all the nesting levels). Don't forget about Exclude.
-                // TODO: inheritance
-                // TODO: aspect on interfaces
-                // TODO: aspect in a referenced assembly (assembly resolver); resolving using configuration from app.config
-                // TODO: handle signed assemblies
-                // TODO: optimize performance of the Builder
-                // TODO: process only once (check if there's Shaspect.Implementation namespace there
-                // TODO: support changing of method arguments (something like args.SetArgument (0, "12345") )
-                // TODO: support async methods
-                // TODO: support aspects on each yield return (now it's only supported as returning of IEnumerable<T> in return value)
-
                 stopwatch.Stop();
                 Log.LogMessage ("ShaspectBuildTask took {0}ms", stopwatch.ElapsedMilliseconds);
 
